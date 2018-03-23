e5ad2f73a5a4d51eadc7dec83f89908d0a0dcfd2
rajapattanayak <pattanayak.raja@gmail.com>
2018-03-23 11:46:16 +0530

    Bug fixes to run code in heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const config   = require('../../config');
 
 mongoose.Promise = global.Promise;
 
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { User }     = require('./models/user');
 
 const app = express();
 
-const port = process.end.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
