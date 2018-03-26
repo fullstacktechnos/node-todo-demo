0a835082064286a24da92fb755b21bd5c2e3167a
rajapattanayak <pattanayak.raja@gmail.com>
2018-03-26 12:37:05 +0530

    TCs for Login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -231,11 +231,13 @@ describe("POST /users/", () => {
       .end(err => {
         if (err) return done(err);
 
-        User.findOne({ email }).then(user => {
+        User.findOne({ email })
+        .then(user => {
           expect(user).toBeTruthy();
           expect(user.password).not.toBe(password);
           done();
-        });
+        })
+        .catch(e => done(e));
       });
   });
 
@@ -261,4 +263,55 @@ describe("POST /users/", () => {
       .expect(400)
       .end(done);
   });
+});
+
+describe("POST /users/login", () => {
+
+  it("should login user and return auth token", done => {
+    const email = users[1].email;
+    const password = users[1].password;
+
+    request(app)
+      .post('/users/login')
+      .send({ email, password })
+      .expect(200)
+      .expect(res => {
+        expect(res.headers["x-auth"]).toBeTruthy();
+        expect(res.body.user.email).toBe(email);
+      })
+      .end((err, res)=> {
+        if (err) return done(err);
+
+        User.findById(users[1]._id)
+        .then(user => {
+          expect(user.tokens[0].token).toBe(res.headers['x-auth']);
+          done();
+        })
+        .catch(e => done(e));
+      });
+  });
+
+  it("should reject invalid login", done => {
+    const email = users[1].email;
+    const password = 'abcdefg123@';
+
+    request(app)
+      .post('/users/login')
+      .send({ email, password })
+      .expect(400)
+      .expect(res => {
+        expect(res.body.error).toBe('Unauthorised');
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+
+        User.findById(users[1]._id)
+        .then(user => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        })
+        .catch(e => done(e));
+      });
+  });
+
 });
